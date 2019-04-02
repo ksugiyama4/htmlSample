dca2543493fbb753ae5d19a34b4ab273ea11944b
ksugiyama4 <ksugiyama4@lenovo.com>
2019-04-02 13:16:25 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -31,4 +31,4 @@ app.get("/api/pokemon/status/:name", function(req, res, next){
             res.send(results[0].pok_index)
     });
 });
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(80, () => console.log('Example app listening on port 80!'))
