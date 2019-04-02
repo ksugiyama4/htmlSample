5018f40ce4587311c1d59a1c61bbde01c9784f6a
ksugiyama4 <ksugiyama4@lenovo.com>
2019-04-02 13:30:38 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -25,10 +25,29 @@ connection.connect(function(err) {
   connection.query('SELECT * FROM `POKEMON_TABLE` WHERE `pok_index` = 1', function (error, results, fields) {
     app.get('/', (req, res) => res.send(myLib.helloWorld() + results[0].pok_name))
 });
-app.get("/api/pokemon/status/:name", function(req, res, next){
+app.get("/api/pokemon/status/:name/:item", function(req, res, next){
     console.log(req.params.name);
     connection.query('SELECT * FROM `POKEMON_TABLE` WHERE `pok_name` = "'+ req.params.name +'"', function (error, results, fields) {
-            res.send(results[0].pok_index)
+        switch(req.params.item) {
+            case 'h':
+            res.send(results[0].pok_h)
+                break;
+            case 'a':
+            res.send(results[0].pok_a)
+                break;
+            case 'b':
+            res.send(results[0].pok_b)
+                break;
+            case 'c':
+            res.send(results[0].pok_c)
+                break;
+            case 'd':
+            res.send(results[0].pok_d)
+                break;
+            case 's':
+            res.send(results[0].pok_s)
+                break;
+        }
     });
 });
 app.listen(80, () => console.log('Example app listening on port 80!'))
